a740e99cfec4db36a8c7a6ace4fcfcb7f251679c
The Blue Chalk <thebluechalk@gmail.com>
2020-09-16 10:35:25 +0530

    finished chapter 6

--- client.py
+++ client.py
@@ -1,12 +1,13 @@
 import socket
 
-s = socket.socket()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 port = 3000
 ip = '127.0.0.1'
 
 s.connect((ip, port))
 
-print(s.recv(1024))
+s.send("GET lmao \n".encode('ascii'))
+print(s.recv(4096).decode('ascii'))
 
 s.close()
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket 
 
-s = socket.socket()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("Socket created")
 
 port = 3000
@@ -10,11 +10,29 @@ s.bind(('', port))
 s.listen(5)
 print("socket is listening")
 
+dictionary = {
+    "aim": "to direct at something"
+}
+found = False
 while True:
     client, address = s.accept()
     print("Got a connection from: ", address)
+    enter = client.recv(4096).decode('ascii')
 
-    client.send(b'thanks for connecting')
+    enter = enter.split()
 
-    client.close
+    if enter[0] == "GET":
+        for word in dictionary:
+            if word == enter[1]:
+                client.send((dictionary.get("aim")).encode('ascii'))
+                found = True
+            else:
+                found = False
+
+        if not found:
+            client.send("Word not found".encode('ascii'))
+
+    
+
+    client.close()
 
